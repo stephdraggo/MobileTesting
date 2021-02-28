12b340aff930f217316aa2195799d6091fbf49dd
stephdraggo <stephdraggo@users.noreply.github.com>
2021-03-01 10:06:10 +1100

    push

--- Assets/Butter/Mobile/MobileInput.cs
+++ Assets/Butter/Mobile/MobileInput.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
-using NullRE = System.NullReferenceException;
-using InvalidOE = System.InvalidOperationException;
+using NullReEx = System.NullReferenceException;
+using InvalidOpEx = System.InvalidOperationException;
 
 namespace BUTTer.Mobile
 {
@@ -12,9 +12,12 @@ namespace BUTTer.Mobile
         private static MobileInput instance = null;
 
         [SerializeField] private JoystickInput joystickInput;
+        [SerializeField] private SwipeInput swipeInput;
         #endregion
 
         #region Methods
+
+        #region initialise
         /// <summary>
         /// If the system is not set up, instantiate mobile input prefab and assign static reference
         /// </summary>
@@ -22,7 +25,7 @@ namespace BUTTer.Mobile
         {
             if (initilised) //if already initialised
             {
-                throw new InvalidOE("Mobile Input already initialised."); //throw error
+                throw new InvalidOpEx("Mobile Input already initialised."); //throw error
             }
 
             MobileInput prefabInstance = Resources.Load<MobileInput>("Mobile Input Prefab"); //load prefab
@@ -31,6 +34,9 @@ namespace BUTTer.Mobile
             instance.gameObject.name = "Mobile Input"; //foolproof naming
             DontDestroyOnLoad(instance.gameObject); //make permanent throughout scenes
         }
+        #endregion
+        
+        #region get joystick
         /// <summary>
         /// return joystick axis value if valid
         /// </summary>
@@ -40,12 +46,12 @@ namespace BUTTer.Mobile
         {
             if (!initilised)//check we have input set up
             {
-                throw new InvalidOE("Mobile Input not initialised.");
+                throw new InvalidOpEx("Mobile Input not initialised.");
             }
 
             if (instance.joystickInput == null) //check joystick is not null
             {
-                throw new NullRE("Joystick Input reference not set.");
+                throw new NullReEx("Joystick Input reference not set.");
             }
 
             switch (_axis)
@@ -59,6 +65,49 @@ namespace BUTTer.Mobile
             }
         }
         #endregion
+
+        #region get swipe
+        /// <summary>
+        /// Attempt to retrieve swipe based on passed finger index.
+        /// </summary>
+        /// <param name="_index">finger index being checked for swipes</param>
+        /// <returns>swipe if not null</returns>
+        public static SwipeInput.Swipe GetSwipe(int _index)
+        {
+            if (!initilised)//check we have input set up
+            {
+                throw new InvalidOpEx("Mobile Input not initialised.");
+            }
+
+            if (instance.swipeInput == null) //check joystick is not null
+            {
+                throw new NullReEx("Swipe Input reference not set.");
+            }
+
+            //go get swipe from swipe input class
+            return instance.swipeInput.GetSwipe(_index);
+        }
+        #endregion
+
+        #region get flick data
+        public static void GetFlickData(out float _flickStrength,out Vector2 _flickDirection)
+        {
+            if (!initilised)//check we have input set up
+            {
+                throw new InvalidOpEx("Mobile Input not initialised.");
+            }
+
+            if (instance.swipeInput == null) //check joystick is not null
+            {
+                throw new NullReEx("Swipe Input reference not set.");
+            }
+
+            _flickStrength = instance.swipeInput.FlickStrength;
+            _flickDirection = instance.swipeInput.FlickDirection;
+        }
+        #endregion
+
+        #endregion
     }
 }
 
@@ -66,7 +115,7 @@ namespace BUTTer.Mobile
  
      
      
-     #region Variables
+    #region Variables
 
     #endregion
 
--- Assets/Butter/Testing/Mobile/MobileTest.cs
+++ Assets/Butter/Testing/Mobile/MobileTest.cs
@@ -6,7 +6,7 @@ using BUTTer.Mobile;
 public class MobileTest : MonoBehaviour
 {
     #region Variables
-
+    [SerializeField] private bool testJoystick = false, testSwipe = false;
     #endregion
 
     #region Start
@@ -19,8 +19,35 @@ public class MobileTest : MonoBehaviour
     #region Update
     void Update()
     {
-        transform.position += transform.forward * MobileInput.GetJoystickAxis(JoystickAxis.Vertical) * Time.deltaTime;
-        transform.position += transform.right * MobileInput.GetJoystickAxis(JoystickAxis.Horizontal) * Time.deltaTime;
+        if (testJoystick)
+        {
+            transform.position += transform.forward * MobileInput.GetJoystickAxis(JoystickAxis.Vertical) * Time.deltaTime;
+            transform.position += transform.right * MobileInput.GetJoystickAxis(JoystickAxis.Horizontal) * Time.deltaTime;
+        }
+
+        if (testSwipe)
+        {
+#if (UNITY_ANDROID || UNITY_IOS) && !UNITY_EDITOR
+
+            //mobile input here
+
+#else
+            if (Input.GetMouseButtonDown(0)) //left click start
+            {
+                
+            }
+            if (Input.GetMouseButton(0)) //left click update
+            {
+
+            }
+            if (Input.GetMouseButtonUp(0)) //left click end
+            {
+                
+            }
+
+            Vector2 touchPos = Input.mousePosition;
+#endif
+        }
     }
     #endregion
 
